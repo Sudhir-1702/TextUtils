424d609f817d658f4f321d602831a796d182f643
Sudhir_1702 <sudhirpanchani99@gmail.com>
2021-09-10 14:04:20 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState('light')
@@ -43,24 +40,24 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
       
-    <Switch>
-          <Route exact path="/about">
+    {/* <Switch> */}
+          {/* <Route exact path="/about">
             <About />
-          </Route>        
-          <Route exact path="/">
+          </Route>         */}
+          {/* <Route exact path="/"> */}
             <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>          
-          </Route>
-    </Switch>
+          {/* </Route> */}
+    {/* </Switch> */}
     
     
     {/* <About/> */}
     </div>
-    </Router>
+    {/* </Router> */}
     </>
     
   );
